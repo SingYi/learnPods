3af84de7d77c17b6f83d348a61efe9bc1f35cdf7
Sing.Yi <23323231@qq.com>
2018-03-20 16:56:07 +0800

    Initial Commit

---
+++ learnPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  learnPods
+//
+//  Created by 燚 on 2018/3/20.
+//  Copyright © 2018年 Sans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ learnPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  learnPods
+//
+//  Created by 燚 on 2018/3/20.
+//  Copyright © 2018年 Sans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
